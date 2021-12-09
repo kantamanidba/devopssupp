de5234bb834138e3f8a2e15df304751feb9fc999
Narayana Kantamani <Narayana Kantamani>
2021-12-09 16:12:25 -0500

    terraform files updated

---
+++ terraform/modules/s3_bucket/outputs.tf
@@ -0,0 +1,11 @@
+output "source_arn" {
+  value = module.s3_bucket.source_arn
+}
+
+output "source_id" {
+  value = module.s3_bucket.source_id
+}
+
+output "bucket_name" {
+  value = module.s3_bucket
+}
